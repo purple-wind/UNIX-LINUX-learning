5c6d695fe838931c838ab15f3f72637692f43b3d
purple-wind <clever_programmer@163.com>
2023-02-10 10:22:50 +0800

    Update sys_call.cpp

--- general/sys_call.cpp
+++ general/sys_call.cpp
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    long ID1;
+
+    ID1 = getpid();
+    printf("pid=%ld\n", ID1);
+
+    long ID2;
+    asm volatile(
+              "int $0x80\n\t"
+              : "=a" (ID2)     /* eax 亦是返回值所在，因此要与ID1 对应 */
+              : "0" (20)         /* 20 置入 eax */
+        );
+
+    printf("pid=%ld\n", ID2);
+
+    return 0;
+}
